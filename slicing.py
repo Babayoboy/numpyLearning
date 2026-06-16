1e73b975ceca12bab01fef7ae6d8981ff447abc5
Ansh joshi <106421634+Babayoboy@users.noreply.github.com>
2026-06-16 17:44:52 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,18 @@
+import numpy as pi
+
+array = pi.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+
+# array[start:end:step]
+
+# print(array[0])  # prints the first row
+# print(array[1]) # prints the secound row
+# print(array[-2])  # prints the last secound row
+
+# print(array[1:]) # prints all the row from 2nd
+# print(array[1:3]) # prints from secound to 3rd
+
+# print(array[0:4:2])  # form 0-3 with skipping 1 row in between
+# print(array[::2])  # if want to select full array with skipping 1 row in between
+
+# print(array[::-1])  # Array in reverse
+# print(array[::-2])  # Array in reverse with skiping one
